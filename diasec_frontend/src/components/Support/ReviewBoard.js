d303e7e626ac36c89c59814d08f729bb17f6018a
Yonic312 <ad3121@naver.com>
2026-07-28 12:30:04 +0900

    260728 리뷰 문구 위치 수정

--- diasec_frontend/src/components/Support/ReviewBoard.js
+++ diasec_frontend/src/components/Support/ReviewBoard.js
@@ -156,14 +156,8 @@ const ReviewBoard = () => {
             <ul className='divide-y border-y'>
                 {currentReviews.map((review, i) => (
                     <li key={i} className='
-                        flex gap-4 py-6 cursor-pointer' onClick={() => setSelectedReview(review)}>
+                        flex gap-4 py-4 cursor-pointer' onClick={() => setSelectedReview(review)}>
                         <div className='flex-1'>
-                            <div className="
-                                md:text-sm text-[clamp(12px,1.825vw,14px)]
-                                flex items-center justify-between text-gray-400">
-                                <span>{review.id.slice(0, 2)}***님</span>
-                                <span>{review.createdAt?.slice(2, 10).replaceAll('-', '.')}</span>
-                            </div>
                             <div className="
                                 md:text-[16px] text-[clamp(12px,2.085vw,16px)]
                                 font-semibold text-gray-700">
@@ -200,6 +194,12 @@ const ReviewBoard = () => {
                                             object-cover rounded border"
                                     />
                                 ))}
+                            </div>
+                            <div className="
+                                md:text-sm text-[clamp(12px,1.825vw,14px)]
+                                flex items-center justify-between text-gray-400 mt-3">
+                                <span>{review.id.slice(0, 2)}***님</span>
+                                <span>{review.createdAt?.slice(2, 10).replaceAll('-', '.')}</span>
                             </div>
                         </div>
                     </li>
